4e4787b5934d5c3413ad5518ea3ae977d047ef2d
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-04-24 10:56:03 -0500

    challenge solved

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-const { StackObj } = require ('./stack-obj');
+const { StackObj } = require('./stack-obj');
+const NUMERIC_SYSTEM = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function baseConverter(decNumber, base) {
-  // your code
-  return ;
+  if (base < 2 || base > 36) return null;
+  const stack = new StackObj();
+  while (decNumber > 0) {
+    stack.push(NUMERIC_SYSTEM[decNumber % base]);
+    decNumber = Math.floor(decNumber / base);
+  }
+
+  let baseNumber = '';
+  while(!stack.isEmpty()){
+    baseNumber += stack.pop();
+  }
+
+  return baseNumber;
 }
 
 module.exports = { baseConverter };
