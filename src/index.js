64d2a08d57eeb7e759efa93144b525d357798255
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-05-13 12:59:27 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-const { StackObj } = require ('./stack-obj');
+const { StackObj } = require('./stack-obj');
 
 function baseConverter(decNumber, base) {
-  // your code
-  return ;
+  const stack = new StackObj();
+  const offsetAscii = 55;
+  let result = '';
+
+  if (base >= 2 && base <= 36) {
+    while (decNumber > 0) {
+      let remainder = decNumber % base;
+      if (remainder > 9) {
+        remainder = String.fromCharCode(remainder + offsetAscii);
+      } else {
+        remainder = remainder.toString();
+      }
+      stack.push(remainder);
+      decNumber = Math.floor(decNumber / base);
+    }
+
+    while (!stack.isEmpty()) {
+      result += stack.pop();
+    }
+  } else {
+    return null;
+  }
+
+  return result;
 }
 
 module.exports = { baseConverter };
