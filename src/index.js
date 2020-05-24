724cc60716d9c695bdc6d635a56f4a19c8dff488
Yair Mendoza <yairmendo@gmail.com>
2020-05-24 13:18:09 -0500

    problem solved using toString() function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const { StackObj } = require ('./stack-obj');
 
-function baseConverter(decNumber, base) {
-  // your code
-  return ;
+function baseConverter(decNumber, base) 
+{
+  if (decNumber < 0 || base < 2 || base > 36)
+    return null;
+  else
+    { 
+      var resultado = decNumber.toString(base);
+      return resultado.toUpperCase();
+    }    
 }
 
 module.exports = { baseConverter };
