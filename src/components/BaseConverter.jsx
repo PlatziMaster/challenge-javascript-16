b7b4e79fb98cc6e6efb80f7e7dfac816582a1b23
Alejandro Lopez <alejozepol@gmail.com>
2020-05-25 21:15:41 -0500

    Inicio base vista en react

---
+++ src/components/BaseConverter.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const BaseConverter = () => (
+  <section className='BaseConverter'>
+    <h2 className='BaseConverter__title' >Convertidor de Bases</h2>
+    <div className='BaseConverter__input'>
+      <div className='BaseConverter__input-radio'>
+        <div className='BaseConverter__input-radio-converter'>
+          <input type='radio' id='converter' name="type" value="1" defaultChecked />
+          <label htmlFor="converter">Convertir</label>
+        </div>
+        <div className='BaseConverter__input-radio-calculator'>
+          <input type='radio' id='calculator' name="type" value="2" />
+          <label htmlFor="calculator">Calculadora</label>
+        </div>
+      </div>
+      <select name="Base" className='BaseConverter__input-base inputDefaul'>
+        <option value="B" defaultValue>Binario</option>
+        <option value="O">Octal</option>
+        <option value="D">Decimal</option>
+        <option value="H">Hexadecimal</option>
+      </select>
+      <div className='BaseConverter__input-n1'>
+        <label htmlFor="N1">Número 1</label>
+        <input type='text' id='N1' name="N1" className='inputDefaul'/>
+      </div>
+      <div className='BaseConverter__input-n2'>
+        <label htmlFor="N2">Número 2</label>
+        <input type='text' id='N2' name="N2" className='inputDefaul' />
+      </div>
+      <div className='BaseConverter__input-operation inputDefaul'>
+        <button>+</button>
+        <button>-</button>
+        <button>x</button>
+        <button>/</button>
+      </div>
+      <button className='BaseConverter__input-btn inputDefaul'>Convertir</button>
+    </div>
+  </section>
+);
+
+export default BaseConverter;
---
+++ src/container/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaseConverter from '../components/BaseConverter'
+import '../assets/app.scss';
+
+const Home = () => {
+  return (
+   <section className='Home'>
+    <BaseConverter />
+   </section>
+  )
+};
+
+export default Home;
